23d055a332c4c1f0f95082eacf763d99362b0667
Zhu <1@qq.com>
2024-10-11 20:42:23 +0800

    first

--- strings/basic.go
+++ strings/basic.go
@@ -1,15 +1,9 @@
 package strings
 
-import "strings"
-
-func IsBlank(s string) bool {
-	return len(strings.TrimSpace(s)) == 0
+func IsBlank(s *string) bool {
+	return s == nil || len(*s) == 0
 }
 
-func IsAbsBlank(s string) bool {
-	return len(s) == 0
-}
-
-func IsNotBlank(s string) bool {
+func IsNotBlank(s *string) bool {
 	return !IsBlank(s)
 }
